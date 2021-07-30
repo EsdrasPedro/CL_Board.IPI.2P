8c54f2d7927354c2cc3d240752a99d37c6890f07
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-07-30 00:44:29 -0300

    CRUD Tabela CL_Usuário.csv

---
+++ CRUD - .csv/CL_Usuário/delete1.php
@@ -0,0 +1,13 @@
+<?php 
+    $rows = file('BD1.CSV');
+    $index = $_GET['line'];
+
+    unset($rows[$index]);
+
+    $new_data = implode('', $rows);
+
+    file_put_contents('BD1.CSV', $new_data);
+
+    header('Location: BD-1.php');
+
+?>
