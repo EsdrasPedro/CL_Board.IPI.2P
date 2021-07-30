5804c342a3ee13628097ffe50c70c1f679f41dd4
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-07-30 01:13:27 -0300

    CRUD / BD - CL_Usuário.html
    Arquivos que fazem o CRUD do arquivo CL_Usuário.html

---
+++ Auth/User/Read.php
@@ -0,0 +1,59 @@
+<?php
+    include '../Connect.php';
+    
+    $stmt = $pdo->query('SELECT * FROM CL_USUARIO');
+    $data = $stmt->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Usuários</title>
+  <style>
+  table, tr, td, th {
+    border: 1px solid hsl(0, 0%, 50%);
+    border-collapse: collapse;
+  }
+  td, th {
+    padding: 0.5em;
+  } 
+  </style>
+</head>
+<body>
+  <h1>Lista de Usuários</h1>
+  <table>
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>SOBRENOME</th>
+        <th>DATA DE NASCIMENTO</th>
+        <th>GENERO</th>
+        <th>N° CELULAR</th>
+        <th>TIPO DE USUARIO</th>
+        <th>MATRICULA</th>
+        <th>E-MAIL</th>
+        <th>SENHA</th>
+    </tr>
+    <?php foreach($data as $row): ?>
+        <tr>
+            <td><?= $row[0] ?></td>
+            <td><?= $row[1] ?></td>
+            <td><?= $row[2] ?></td>
+            <td><?= $row[3] ?></td>
+            <td><?= $row[4] ?></td>
+            <td><?= $row[5] ?></td>
+            <td><?= $row[6] ?></td>
+            <td><?= $row[7] ?></td>
+            <td><?= $row[8] ?></td>
+            <td><?= $row[9] ?></td>
+            <td>
+                <a href="Remove.php?ID=<?= $row[0] ?>">Remover</a>
+            </td>
+        </tr>
+    <?php endforeach ?>
+  </table>
+</body>
+</html>
---
+++ Auth/User/Save.php
@@ -0,0 +1,24 @@
+<?php
+    include '../Connect.php';
+
+    $nome1 = $_POST['fName'];
+    $nome2 = $_POST['lName'];
+    $nascer = $_POST['Nascimento'];
+    $genero = $_POST['country'];
+    $celular = $_POST['Celular'];
+    $tipe1 = $_POST['country2'];
+    $matricula = $_POST['Matricula'];
+    $email = $_POST['email'];
+    $senha = $_POST['password1']; 
+    $senha2 = $_POST['password2'];
+
+    if ($senha != $senha2) {
+        header('location: /CL_usuário.html');
+        exit();
+    }
+
+    $stmt = $pdo->prepare("INSERT INTO CL_USUARIO (NOME, SOBRENOME, DATA_DE_NASCIMENTO, GENERO, N_CELULAR, TIPO_USUARIO, MATRICULA, E_MAIL, SENHA) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->execute([$nome1, $nome2, $nascer, $genero, $celular, $tipe1, $matricula, $email, $senha]);
+
+    header('Location: /Login.php');
+?>
