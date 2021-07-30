589e076c598fb373de14256fe88771720efff743
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-07-30 01:41:01 -0300

    Connect.php
    Arquivos para referencias o banco de dados.

---
+++ Auth/Connect.php
@@ -0,0 +1,19 @@
+<?php
+
+    //$pdo = new PDO('mysql:host=sql10.freemysqlhosting.net;dbname=sql10423959', 'sql10423959', 'veUWr6HNrm');
+    //$pdo = new PDO('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'Alessandra1-');
+    $pdo = new PDO('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'Ipi2020');
+
+    session_start();
+
+    function is_logged() {
+        if (isset($_SESSION['CL_USUARIO_ID'])){
+            return true;
+        }
+        return false;
+    }
+
+    function user() {
+        return $_SESSION['NOME'] ?? false;
+    }
+?>
