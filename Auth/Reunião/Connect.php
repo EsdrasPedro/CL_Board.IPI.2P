e97aad8a22c3a6567b240a82605188f1ecd8cc5f
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-07-31 10:27:07 -0300

    Alterando os arquivos Connect

--- Auth/Reunião/Connect.php
+++ Auth/Reunião/Connect.php
@@ -1,8 +1,10 @@
 <?php
 
     //$pdo = new PDO('mysql:host=sql10.freemysqlhosting.net;dbname=sql10423959', 'sql10423959', 'veUWr6HNrm');
-    /*$pdo = new PDO ('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'daniel333');*/
+    //$pdo = new PDO('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'Alessandra1-');
+    //$pdo = new PDO ('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'daniel333');
     $pdo = new PDO('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'Ipi2020');
+
     session_start();
 
     function is_logged() {
@@ -12,7 +14,7 @@
         return false;
     }
 
-    function NOME() {
+    function user() {
         return $_SESSION['NOME'] ?? false;
-    } 
+    }
 ?>
