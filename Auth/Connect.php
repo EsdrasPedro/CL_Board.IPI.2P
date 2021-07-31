8df3a8904413ca3b614e6d8513c1cdfde5e823c0
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-07-31 01:10:08 -0300

    Update de arquivo
    update de arquivo html para php
    linha de codigo comentada no arquivo Connect.php

--- Auth/Connect.php
+++ Auth/Connect.php
@@ -2,6 +2,8 @@
 
     //$pdo = new PDO('mysql:host=sql10.freemysqlhosting.net;dbname=sql10423959', 'sql10423959', 'veUWr6HNrm');
     //$pdo = new PDO('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'Alessandra1-');
+    /*$pdo = new PDO ('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'daniel333');*/
+
     $pdo = new PDO('mysql:host=localhost;dbname=cl_board;port=3306', 'root', 'Ipi2020');
 
     session_start();
