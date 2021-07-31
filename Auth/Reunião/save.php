be0923ee4fe13236562297423f3ba43fcdf1df2f
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-07-31 03:32:22 -0300

    Update save.php
    extensão do href alterada para .php

--- Auth/Reunião/save.php
+++ Auth/Reunião/save.php
@@ -5,9 +5,10 @@
     $Name = $_POST['NameMateria'];
     $D_Inicio = $_POST['Date_Ini'];
     $D_Fim = $_POST['Date_Fim'];
+    $Select_D = $_POST['select_disciplina'];
 
-    $stmt = $pdo->prepare("INSERT INTO CL_REUNIÃO_MARCADA (NOME, DATA_H_INICIO, DATA_H_FIM) VALUES (?, ?, ?)");
-    $stmt->execute([$Name, $D_Inicio, $D_Fim]);
+    $stmt = $pdo->prepare("INSERT INTO CL_REUNIÃO_MARCADA (NOME, DATA_H_INICIO, DATA_H_FIM, DIO_ID) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$Name, $D_Inicio, $D_Fim, $Select_D]);
 
     header('Location: /index.php');
 
