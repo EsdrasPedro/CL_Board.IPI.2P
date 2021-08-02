2f5154f60f3d41f7ddb1b27fe21423b612caa1b6
Ingrid-st <ingridfarias161@gmail.com>
2021-08-02 14:28:25 -0300

    Alteração e inclusão das informações do Select Opition do formulário
    No arquivo Save da Disciplina:     $stmt = $pdo->prepare("INSERT INTO CL_DISCIPLINA (CODIGO, NOME, CUR_ID) VALUES (?, ?, ?)");
        $stmt->execute([$nome2, $nome1, $cur]);

--- Auth/Disciplina/Save.php
+++ Auth/Disciplina/Save.php
@@ -3,10 +3,11 @@
 
     $nome1 = $_POST['DitName'];
     $nome2 = $_POST['codig'];
+    $cur = $_POST['select_curso'];
 
 
-    $stmt = $pdo->prepare("INSERT INTO CL_DISCIPLINA (CODIGO, NOME) VALUES (?, ?)");
-    $stmt->execute([$nome2, $nome1]);
+    $stmt = $pdo->prepare("INSERT INTO CL_DISCIPLINA (CODIGO, NOME, CUR_ID) VALUES (?, ?, ?)");
+    $stmt->execute([$nome2, $nome1, $cur]);
 
     header('Location: /CL_disciplina_ofertada.php');
 ?>
