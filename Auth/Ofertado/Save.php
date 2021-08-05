6837a5f7ec42af74c1470f463dd14b2d97f3d261
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-08-05 01:15:08 -0300

    save.pho disciplina ofertada
    Uptade de variáveis

--- Auth/Ofertado/Save.php
+++ Auth/Ofertado/Save.php
@@ -2,11 +2,13 @@
     include '../Connect.php';
 
     $ano = $_POST['country1']; 
-    $semestre = $_POST['country2'];  
-    $cor = $_POST['country3'];  
+    $semestre = $_POST['country2'];
+    $cor = $_POST['country3']; 
+    $dis = $_POST['dis_id'];
+    $id = $_SESSION['CL_USUARIO_ID'];
 
-    $stmt = $pdo->prepare("INSERT INTO CL_DISCIPLINA_OFERTADA (ANO, SEMESTRE, COR_DISC) VALUES (?, ?, ?)");
-    $stmt->execute([$ano, $semestre, $cor]);
+    $stmt = $pdo->prepare("INSERT INTO CL_DISCIPLINA_OFERTADA (ANO, SEMESTRE, COR_DISC, DIS_ID, USU_ID_PROFESSOR) VALUES (?, ?, ?, ?, ?)");
+    $stmt->execute([$ano, $semestre, $cor, $dis, $id]);
 
     header('Location: /index.php');
 ?>
