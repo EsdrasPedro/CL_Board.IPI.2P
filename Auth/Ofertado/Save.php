71c0fd4c1adae56f4a10fdd14f35bad8b6d8c387
Ingrid-st <ingridfarias161@gmail.com>
2021-08-05 16:49:58 -0300

    Modificando o Save de Ofertado
    Excluí o "$dis = $_POST['dis_id'];" que fazia jus ao input e adicionei o "$select_dis = $_POST['select_disciplina'];" para ser coerente com as novas modificações do CL_disciplina_ofertada (dentro da div com Selecionar Disciplina e o foreach).

--- Auth/Ofertado/Save.php
+++ Auth/Ofertado/Save.php
@@ -4,11 +4,12 @@
     $ano = $_POST['country1']; 
     $semestre = $_POST['country2'];
     $cor = $_POST['country3']; 
-    $dis = $_POST['dis_id'];
+    $select_dis = $_POST['select_disciplina']; 
     $id = $_SESSION['CL_USUARIO_ID'];
 
     $stmt = $pdo->prepare("INSERT INTO CL_DISCIPLINA_OFERTADA (ANO, SEMESTRE, COR_DISC, DIS_ID, USU_ID_PROFESSOR) VALUES (?, ?, ?, ?, ?)");
-    $stmt->execute([$ano, $semestre, $cor, $dis, $id]);
+    $stmt->execute([$ano, $semestre, $cor, $select_dis, $id]);
 
     header('Location: /index.php');
 ?>
+
