a1f697ee7d1f65b8aa5ef2870557eb6df0b9e809
Esdras Pedro <esdraspedro@hotmail.com.br>
2021-08-14 02:21:32 -0300

    Adicionando novos arquivos

---
+++ CL_Curso.php
@@ -0,0 +1,187 @@
+<?php
+    include 'Connect.php';
+
+    if (!is_logged()) {
+      header('location: Login.php');
+      exit();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="PageIndex.css" rel="stylesheet">
+    <script src="#"></script>
+    <style>
+      .form-label {
+        margin-bottom: .5rem;
+        display: inline-block;
+      }
+      .col-sm-6 {
+        flex: 0 0 auto;
+        width: 40%;
+        flex-wrap: wrap;
+      }
+
+      .input-field {
+        font-family: inherit;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: inherit;
+        width: 100%;
+        height: auto;
+        padding: 0.55rem 1.25rem;
+        border: none;
+        outline: none;
+        border-radius: 2rem;
+        color: #121212;
+        background: #e6eef9;
+        text-transform: unset;
+        text-rendering: optimizeLegibility;
+        font-family: "DM Sans", sans-serif;
+      }
+      .input-submit {
+        font-family: inherit;
+        font-size: 1rem;
+        font-weight: 500;
+        line-height: inherit;
+        cursor: pointer;
+        min-width: 40%;
+        height: auto;
+        padding: 0.75rem 1.25rem;
+        border: none;
+        outline: none;
+        border-radius: 2rem;
+        color: #ffffff;
+        background: #1f1c24;
+        text-transform: capitalize;
+        text-rendering: optimizeLegibility;
+        margin-top: 277px;
+        margin-left: 550px;
+      }
+    </style>
+    <title>Novo</title>
+</head>
+<body>
+    <div class="app-container">
+        <div class="app-header">
+
+          <div class="app-header-left">
+            <span class="app-icon"></span>
+            <p class="app-name">CL Board</p>
+
+            <div class="search-wrapper">
+              <input class="search-input" type="text" placeholder="Buscar">
+              <svg width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-search" viewBox="0 0 24 24">
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="M21 21l-4.35-4.35"></path>
+              </svg>
+            </div>
+
+          </div>
+
+          <div class="app-header-right">
+
+            <button class="notification-btn">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
+                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+                <path d="M13.73 21a2 2 0 0 1-3.46 0" /></svg>
+            </button>
+
+            <button class="profile-btn">
+                <img src="https://assets.codepen.io/3306515/IMG_2025.jpg">
+                <span> <?= user() ?> </span>
+            </button>
+
+          </div>
+        </div>
+        <div class="app-content">
+
+          <div class="app-sidebar">
+
+            <a href="" class="app-sidebar-link">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+          <polyline points="9 22 9 12 15 12 15 22" /></svg>
+      </a>
+      <a href="" class="app-sidebar-link">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
+          <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+          <line x1="16" y1="2" x2="16" y2="6" />
+          <line x1="8" y1="2" x2="8" y2="6" />
+          <line x1="3" y1="10" x2="21" y2="10" /></svg>
+      </a>
+      <a href="" class="app-sidebar-link active">
+        <svg class="btn-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
+          <line x1="12" y1="5" x2="12" y2="19" />
+          <line x1="5" y1="12" x2="19" y2="12" /></svg>
+      </a>
+      <a href="" class="app-sidebar-link">
+        <svg class="link-icon" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-settings" viewBox="0 0 24 24">
+          <defs/>
+          <circle cx="12" cy="12" r="3" />
+          <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
+        </svg>
+      </a>
+      <a href="" class="btn-logout">
+        <svg width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-log-out" viewBox="0 0 24 24">
+          <defs></defs>
+          <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9"></path>
+        </svg>
+      </a>
+
+          </div>
+
+
+        <div class="projects-section">
+
+
+          <div class="messages-section1">
+
+            <div class="task-tools">
+                <div>
+                </div>
+                <div>
+                  <button class="task-filter" data-param="completed" data-value="false"></button>
+                </div>
+              </div>
+
+            <div class="projects-section-header">
+                <p>Cadastrar Curso</p>
+            </div>
+
+            <form role="form" action="./Auth/Ofertado/Save.php" method="POST" novalidate>
+
+            <div class="project-boxe"> 
+                    <div class="message-content1">
+                      <div>
+                        <div class="message-header col-sm-6">
+                          <div class="form-label"> Nome </div>
+                          <input type="text" class="input-field" name="fName" placeholder="Nome" required>
+                        </div>
+                        
+                        <div class="position-absolute bottom-0 end-0">
+                          <div>
+                            <button class="task-filter1" data-param="completed" data-value="false">Confirmar</button>
+                          </div>
+                        </div>
+                      </div>
+                  </div>
+            </div>
+
+            </form>
+
+          </div>
+
+        </div>
+
+
+      </div>
+      </div>
+      </div>
+</body>
+</html>
---
+++ CL_Usuario.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="CL_U.css" rel="stylesheet">
+    <title>Criar Conta CL Board</title>
+</head>
+<body class="bg-light">
+  
+  <div class="container">
+    <main>
+      <div class="py-5 text-center">
+        <h2>CL Board</h2>
+        <p class="lead">Serviço de organização de reuniões por vídeo para instituições de ensino, empresas e outros grupos. <br>O CL Board foi projetado tendo como base o Google Classroom e o Kanban, contendo características de ambos, visando facilidade e informação.</p>
+      </div>
+
+        <div class="col-md-7 col-lg-8 wrapper">
+          <h4 class="mb-3">Criar Conta</h4>
+          <form action="./Auth/User/Save.php" method="POST" novalidate>
+            <div class="row g-3">
+              <div class="col-sm-6">
+                <label class="form-label">Nome</label>
+                <input type="text" class="input-field" name="fName" placeholder="Nome" required>
+                <div class="invalid-feedback">O primeiro nome válido é obrigatório.</div>
+              </div>
+  
+              <div class="col-sm-6">
+                <label class="form-label">Sobrenome</label>
+                <input type="text" class="input-field" name="lName" placeholder="Sobrenome" required>
+                <div class="invalid-feedback">O sobrenome nome válido é obrigatório.</div>
+              </div>
+  
+              <div class="col-md-4">
+                <label class="form-label">Gênero</label>
+                <select class="form-select" name="country" required>
+                  <option selected disabled value="">Escolher...</option>
+                  <option value="Masculino">Masculino</option>
+                  <option value="Feminino">Feminino</option>
+                </select>
+                <div class="invalid-feedback">Por favor, selecione um estado válido.</div>
+              </div>
+  
+              <div class="col-md-4">
+                <label class="form-label">Celular</label>
+                <input type="text" class="input-field" name="Celular" placeholder="00000-0000" required>
+                <div class="invalid-feedback">Um número válido é obrigatório.</div>
+              </div>
+
+              <div class="col-md-4">
+                <label class="form-label">Nascimento</label>
+                <input type="date" class="input-field" name="Nascimento" placeholder="DD/MM/AAAA" required>
+              </div>
+
+              <div class="col-md-6">
+                <label class="form-label">Tipo de usuário</label>
+                <select class="form-select" name="country2" required>
+                  <option selected disabled value="">Escolher...</option>
+                  <option value="Aluno">Aluno(a)</option>
+                  <option value="Professor">Professor(a)</option>
+                </select>
+                <div class="invalid-feedback">Por favor, selecione um estado válido.</div>
+              </div>
+
+              <div class="col-md-6">
+                <label class="form-label">Matrícula</label>
+                <input type="text" class="input-field" name="Matricula" required>
+              </div>
+
+              <div class="col-12">
+                <label class="form-label">E-mail</label>
+                <input type="email" class="input-field" name="email" aria-describedby="email1Help" placeholder="name@example.com" required>
+                <div name="email1Help" class="form-text">Nunca compartilharemos seu e-mail com mais ninguém.</div>
+                <div class="invalid-feedback">Por favor, insira um endereço de e-mail válido para atualizações de envio.</div>
+              </div>
+  
+              <div class="col-12">
+                <label class="form-label">Senha</label>
+                <input type="password" class="input-field" name="password1" aria-describedby="passwordHelpBlock" placeholder="Senha" required>
+                <div name="password1Help" class="form-text">Sua senha deve ter de 8 a 20 caracteres, conter letras e números e não conter espaços, caracteres especiais ou emojis.</div>
+                <div class="invalid-feedback">Por favor, insira uma senha válido.</div>
+              </div>
+  
+              <div class="col-12">
+                <label class="form-label">Repetir senha</label>
+                <input type="password" class="input-field" name="password2" placeholder="Senha" required>
+                <div class="invalid-feedback">Por favor, insira uma senha válido.</div>
+              </div>
+
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="same-address">
+                <label class="form-check-label">Quero receber informações pelo e-mail cadastrado.</label>
+              </div>
+    
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="save-info">
+                <label class="form-check-label">Quero receber notificações pelo WhatsApp.</label>
+              </div>
+
+              <div class="var1"></div>
+  
+            <button class="input-submit" type="submit">Finalizar</button>
+          </form>
+        </div>
+      </div>
+
+      <div class="var">
+          
+      </div>
+
+    </main>
+
+  </div>
+</body>
+</html>
