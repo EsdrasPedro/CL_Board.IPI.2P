68056119b05c19d52a37310fafce551ec7d6029a
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-08-14 03:04:51 -0300

    Alterando chamadas para os arquivos css

--- CL_reuniao_marcada.php
+++ CL_reuniao_marcada.php
@@ -22,8 +22,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="PageIndex.css" rel="stylesheet">
-    <link href="CL_U.css" rel="stylesheet">
+    <link href="./Assets/Css/PageIndex.css" rel="stylesheet">
+    <link href="./Assets/Css/CL_U.css" rel="stylesheet">
     <script src="#"></script>
     <style>
 
