e424a2708e3eea7411d703693168197d3e66848c
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-08-15 01:45:39 -0300

    Criação de Variável de referentes ao banco e alteração no redirecionamento de páginas

--- Auth/Reunião/save.php
+++ Auth/Reunião/save.php
@@ -3,14 +3,15 @@
     include 'Connect.php';
 
     $Name = $_POST['NameMateria'];
+    $R_Data = $_POST['DataReuniao']
     $D_Inicio = $_POST['Date_Ini'];
     $D_Fim = $_POST['Date_Fim'];
     $Select_D = $_POST['select_disciplina'];
 
-    $stmt = $pdo->prepare("INSERT INTO CL_REUNIÃO_MARCADA (NOME, DATA_H_INICIO, DATA_H_FIM, DIO_ID) VALUES (?, ?, ?, ?)");
-    $stmt->execute([$Name, $D_Inicio, $D_Fim, $Select_D]);
+    $stmt = $pdo->prepare("INSERT INTO CL_REUNIÃO_MARCADA (NOME, DATA, DATA_H_INICIO, DATA_H_FIM, DIO_ID) VALUES (?, ?, ?, ?, ?)");
+    $stmt->execute([$Name, $R_Data, $D_Inicio, $D_Fim, $Select_D]);
 
-    header('Location: /index.php');
+    header('Location: /Listar_Reuniao_Marcada.php');
 
 
 ?>
