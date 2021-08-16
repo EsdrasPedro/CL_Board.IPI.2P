c854d6997a5bcb715fba1fd80b581de6090571e8
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-08-16 01:14:32 -0300

    Alterações na sintaxe do código

--- Auth/Reunião/save.php
+++ Auth/Reunião/save.php
@@ -3,12 +3,12 @@
     include 'Connect.php';
 
     $Name = $_POST['NameMateria'];
-    $R_Data = $_POST['DataReuniao']
+    $R_Data = $_POST['DataReuniao'];
     $D_Inicio = $_POST['Date_Ini'];
     $D_Fim = $_POST['Date_Fim'];
     $Select_D = $_POST['select_disciplina'];
 
-    $stmt = $pdo->prepare("INSERT INTO CL_REUNIÃO_MARCADA (NOME, DATA, DATA_H_INICIO, DATA_H_FIM, DIO_ID) VALUES (?, ?, ?, ?, ?)");
+    $stmt = $pdo -> prepare("INSERT INTO CL_REUNIÃO_MARCADA (NOME, DATA, H_INICIO, H_FIM, DIO_ID) VALUES (?, ?, ?, ?, ?)");
     $stmt->execute([$Name, $R_Data, $D_Inicio, $D_Fim, $Select_D]);
 
     header('Location: /Listar_Reuniao_Marcada.php');
