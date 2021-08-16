97495cd9b7d2aacb72c52c65030d1318c68e4994
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-08-16 01:18:00 -0300

    Ajustes no redirecionamentos dos arquivos

--- CL_disciplina.php
+++ CL_disciplina.php
@@ -22,6 +22,7 @@
     <link href="./Assets/Css/CL_U.css" rel="stylesheet">
     <script src="#"></script>
     <style>
+
       .form-label {
         margin-bottom: .5rem;
         display: inline-block;
@@ -49,6 +50,7 @@
         text-rendering: optimizeLegibility;
         font-family: "DM Sans", sans-serif;
       }
+
       .input-submit1 {
         font-family: inherit;
         font-size: 1rem;
@@ -148,19 +150,19 @@
 
           <div class="app-sidebar">
 
-            <a href="" class="app-sidebar-link">
+            <a href="Home.php" class="app-sidebar-link">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
           <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
           <polyline points="9 22 9 12 15 12 15 22" /></svg>
       </a>
-      <a href="" class="app-sidebar-link">
+      <a href="Agenda.php" class="app-sidebar-link">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
           <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
           <line x1="16" y1="2" x2="16" y2="6" />
           <line x1="8" y1="2" x2="8" y2="6" />
           <line x1="3" y1="10" x2="21" y2="10" /></svg>
       </a>
-      <a href="" class="app-sidebar-link active">
+      <a href="Listar_Curso.php" class="app-sidebar-link active">
         <svg class="btn-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
           <line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" /></svg>
@@ -172,7 +174,7 @@
           <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
         </svg>
       </a>
-      <a href="" class="btn-logout">
+      <a href="./Auth/Login/Logout.php" class="btn-logout">
         <svg width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-log-out" viewBox="0 0 24 24">
           <defs></defs>
           <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9"></path>
@@ -202,6 +204,7 @@
             <form class="needs-validation" role="form" action="./Auth/Disciplina/Save.php" method="POST" novalidate>
 
             <div class="project-boxe"> 
+
                     <div class="message-content2">
                         <div>
                             <div class="message-header col-sm-6 d">
@@ -210,14 +213,18 @@
                             </div>
 
             <div class="project-boxe"> 
+
                     <div class="message-content3">
                         <div>
                             <div class="message-header col-sm-6 d">
+
                                   <div class="form-label">Código</div>
                                   <input type="text" class="input-field" name="codig" placeholder="Código" required>  
+
                             </div>
 
                             <div class="message-header col-sm-6 d row1">
+
                               <label class="form-label">Selecionar Curso</label>
                               <select class="form-select" name="select_curso" required>
                                 <option selected disabled value="">Escolher...</option>
@@ -227,14 +234,20 @@
                                 <?php endforeach ?>
 
                               </select>
+
                           </div>
 
                             <div class="position-absolute bottom-0 end-0">
+
                               <div>
                               </div>
+
                               <div>
+
                                 <button class="task-filter1" data-param="completed" data-value="false">Confirmar</button>
+                              
                               </div>
+
                             </div>
                           </div>
                       </div>
