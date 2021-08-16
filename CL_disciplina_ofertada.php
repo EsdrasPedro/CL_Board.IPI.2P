8eb77dfdc99319bc20698bfbb00b4c2710605426
Gjss333 <77823704+Gjss333@users.noreply.github.com>
2021-08-16 01:19:22 -0300

    Alterações no redirecionamentos dos arquivos

--- CL_disciplina_ofertada.php
+++ CL_disciplina_ofertada.php
@@ -22,10 +22,12 @@
     <link href="./Assets/Css/CL_U.css" rel="stylesheet">
     <script src="#"></script>
     <style>
+
       .form-label {
         margin-bottom: .5rem;
         display: inline-block;
       }
+
       .col-sm-6 {
         flex: 0 0 auto;
         width: 40%;
@@ -49,6 +51,7 @@
         text-rendering: optimizeLegibility;
         font-family: "DM Sans", sans-serif;
       }
+
       .input-submit1 {
         font-family: inherit;
         font-size: 1rem;
@@ -178,19 +181,19 @@
 
           <div class="app-sidebar">
 
-            <a href="" class="app-sidebar-link">
+            <a href="Home.php" class="app-sidebar-link">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
           <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
           <polyline points="9 22 9 12 15 12 15 22" /></svg>
       </a>
-      <a href="" class="app-sidebar-link">
+      <a href="Agenda.php" class="app-sidebar-link">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
           <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
           <line x1="16" y1="2" x2="16" y2="6" />
           <line x1="8" y1="2" x2="8" y2="6" />
           <line x1="3" y1="10" x2="21" y2="10" /></svg>
       </a>
-      <a href="" class="app-sidebar-link active">
+      <a href="Listar_Curso.php" class="app-sidebar-link active">
         <svg class="btn-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
           <line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" /></svg>
@@ -202,7 +205,7 @@
           <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
         </svg>
       </a>
-      <a href="" class="btn-logout">
+      <a href="./Auth/Login/Logout.php" class="btn-logout">
         <svg width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-log-out" viewBox="0 0 24 24">
           <defs></defs>
           <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h4M16 17l5-5-5-5M21 12H9"></path>
@@ -234,6 +237,7 @@
               <input type="hidden" name="dis_id" value=" <?= $_GET['dis_id'] ?> ">
 
             <div class="project-boxe"> 
+
                     <div class="message-content2">
                         <div>
 
@@ -241,7 +245,9 @@
 
                                 <div class="message-header1 col-sm-3 d esp">
                                     <label class="form-label">Ano</label>
+
                                     <select class="form-select" name="country1" required>
+
                                         <option selected disabled value="">Escolher...</option>
                                         <option value="2014">2014</option>
                                         <option value="2015">2015</option>
@@ -251,25 +257,31 @@
                                         <option value="2019">2019</option>
                                         <option value="2020">2020</option>
                                         <option value="2021">2021</option>
+
                                     </select>
+
                                 </div>
 
                                 <div class="message-header1 col-sm-3">
                                     <label class="form-label">Semestre</label>
+
                                     <select class="form-select" name="country2" required>
                                         <option selected disabled value="">Escolher...</option>
                                         <option value="1">1</option>
                                         <option value="2">2</option>
                                     </select>
+
                                 </div>
 
                             </div>
 
                             <div class="row1">
+
                                 <div class="message-header1 col-sm-1 d">
                                     <label class="form-label">Cor</label>
                                     <input type="color" class="form-control1 form-control-color" name="country3" list="arcoIris" value="#FF0000" required>
                                     <datalist name="country3" required>
+
                                         <option value="#FF0000">Vermelho</option>
                                         <option value="#FFA500">Laranja</option>
                                         <option value="#FFFF00">Amarelo</option>
@@ -277,11 +289,13 @@
                                         <option value="#0000FF">Azul</option>
                                         <option value="#4B0082">Indigo</option>
                                         <option value="#EE82EE">Violeta</option>
-                                      </datalist>
+
+                                    </datalist>
                                 </div>
                             </div>
 
                             <div class="message-header col-sm-6 d row1">
+
                                 <label class="form-label">Selecionar Disciplina</label>
                                 <select class="form-select" name="select_disciplina" required>
                                   <option selected disabled value="">Escolher...</option>
@@ -294,12 +308,17 @@
                             </div>
         
                             <div class="position-absolute bottom-0 end-0">
+
                                 <div>
                                 </div>
+
                                 <div>
+
                                   <button class="task-filter1" data-param="completed" data-value="false">Confirmar</button>
+                               
                                 </div>
-                              </div>
+
+                            </div>
                       </div>
                   </div>
             </div>
